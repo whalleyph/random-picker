3125e16449ab537265109cd301b0093203dc529d
2405-011-pw <walypa56@gmail.com>
2024-07-01 14:04:03 +0100

    finish random pick

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [pickedWord, setPickedWord] = React.useState("Random Word")
+  const [pickedWord, setPickedWord] = React.useState("Random Word");
+  const [textArray, setTextArray] = React.useState([
+    "player1",
+    "player2",
+    "player3",
+    "player4",
+  ]);
+  const textValue = textArray.join(" ");
+
+  function handleChange(event) {
+    const textToArray = event.target.value.split(" ");
+    setTextArray(textToArray);
+  }
+
+  function randomPick() {
+    const randomNumber = Math.floor(Math.random() * textArray.length);
+    setPickedWord(textArray[randomNumber]);
+  }
 
   return (
     <>
       <h1>Random Picker</h1>
       <div className="text--area">
-        <textarea></textarea>
+        <textarea value={textValue} onChange={handleChange} />
       </div>
       <div className="button">
-        <button>Random</button>
+        <button onClick={randomPick}>Random</button>
       </div>
       <div className="picked--word">
         <p>{pickedWord}</p>
