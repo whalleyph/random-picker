cd94f34e63311575b0c1c39f286b90739cbc140a
2405-011-pw <walypa56@gmail.com>
2024-07-01 11:28:17 +0100

    setup HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react"
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [pickedWord, setPickedWord] = React.useState("Random Word")
+
   return (
     <>
       <h1>Random Picker</h1>
-      
+      <div className="text--area">
+        <textarea></textarea>
+      </div>
+      <div className="button">
+        <button>Random</button>
+      </div>
+      <div className="picked--word">
+        <p>{pickedWord}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
